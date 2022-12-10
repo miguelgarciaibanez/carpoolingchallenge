e2bf733bfb42b0c29d418cb0c93d3e53ff29b210
miguelgarciaibanez <mgarciaibanez@gmail.com>
2022-12-10 13:49:21 +0100

    Rise up coverage

---
+++ src/api/controllers/__tests__/cars.spects.ts
@@ -0,0 +1,114 @@
+import request from 'supertest';
+import { Express } from 'express-serve-static-core';
+import { createServer, closeServer } from '@carpool/utils/server';
+import { StatusCodes } from 'http-status-codes';
+import * as CarsService from '@carpool/api/services/cars';
+
+let server: Express;
+
+beforeAll(async()=>{
+    server = await createServer();
+    jest.useFakeTimers();
+})
+
+afterAll(()=>{
+    closeServer();
+});
+
+describe('CARS controller',()=>{
+    it ('should return 200', async()=>{
+        const data=[
+            {
+              "id": 1,
+              "seats": 3
+            },
+            {
+              "id": 2,
+              "seats": 3
+            },
+            {
+              "id": 3,
+              "seats": 2
+            },
+            {
+              "id": 4,
+              "seats": 3
+            },
+            {
+              "id": 5,
+              "seats": 5
+            }
+          ];
+        jest.spyOn(CarsService,'resetApp').mockReturnValueOnce(true);
+        const result = await request(server)
+                                .put('/cars')
+                                .send(data)
+                                .set('Accept', 'application/json');
+        expect(result.status).toEqual(StatusCodes.OK);
+    });
+    
+    it ('should return BAD REQUEST', async()=>{
+        const data=[
+            {
+              "id": 1,
+              "seats": 3
+            },
+            {
+              "id": 2,
+              "seats": 3
+            },
+            {
+              "id": 3,
+              "seats": 2
+            },
+            {
+              "id": 4,
+              "seats": 3
+            },
+            {
+              "id": 5,
+              "seats": 5
+            }
+          ];
+        jest.spyOn(CarsService,'resetApp').mockReturnValueOnce(false);
+        const result = await request(server)
+                                .put('/cars')
+                                .send(data)
+                                .set('Accept', 'application/json');
+        expect(result.status).toEqual(StatusCodes.BAD_REQUEST);
+    });
+    
+
+    it ('should return BAD REQUEST due to exception', async()=>{
+        const data=[
+            {
+              "id": 1,
+              "seats": 3
+            },
+            {
+              "id": 2,
+              "seats": 3
+            },
+            {
+              "id": 3,
+              "seats": 2
+            },
+            {
+              "id": 4,
+              "seats": 3
+            },
+            {
+              "id": 5,
+              "seats": 5
+            }
+          ];
+        jest.spyOn(CarsService,'resetApp').mockImplementation(()=>{
+            throw new Error();
+        });
+        const result = await request(server)
+                                .put('/cars')
+                                .send(data)
+                                .set('Accept', 'application/json');
+        expect(result.status).toEqual(StatusCodes.BAD_REQUEST);
+    });
+})
--- src/api/controllers/cars.ts
+++ src/api/controllers/cars.ts
@@ -10,11 +10,11 @@ export function cars(req: express.Request, res:express.Response):void {
             writeJsonResponse(res,StatusCodes.OK,'');
         }
         else {
-            writeJsonResponse(res, StatusCodes.BAD_REQUEST, 'Bad Request');
+            writeJsonResponse(res, StatusCodes.BAD_REQUEST, '');
         }
     } catch (error) {
         console.log('Error resetting app');
-        writeJsonResponse(res, StatusCodes.BAD_REQUEST, 'Bad Request');
+        writeJsonResponse(res, StatusCodes.BAD_REQUEST, '');
     }
     
 }
